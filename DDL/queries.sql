bb9f6a1b986a5643290267cc8692f6c12f5a0077
Amin Mustafa Boankod <amin.m.boankod@gmail.com>
2023-01-27 18:55:20 +0100

    update(Readme)

--- DDL/queries.sql
+++ DDL/queries.sql
@@ -32,9 +32,6 @@ SELECT user_name FROM user WHERE user_ID IN (SELECT user_ID FROM lending WHERE d
 /* What is the number of books published by the author with ID = 1? */
 SELECT COUNT(*) FROM book WHERE author_ID = 1;
 
-/* What is the name of the users with an outstanding loan? */
-SELECT user_name FROM user WHERE user_ID IN (SELECT user_ID FROM lending WHERE dateReturn IS NULL);
-
 /* What are the titles of the books in aisle D-5? */
 SELECT title FROM book WHERE book_ID IN (SELECT book_ID FROM library WHERE library_location = 'aisle D-5');
 
@@ -47,17 +44,5 @@ SELECT author_name FROM author WHERE author_ID IN (SELECT author_ID FROM book WH
 /* What are the titles of the books published by Andrew Hodges that are in the library? */
 SELECT title FROM book WHERE author_ID = (SELECT author_ID FROM author WHERE author_name = 'Andrew Hodges') AND book_ID IN (SELECT book_ID FROM library);
 
-/* What is the name of the users with an outstanding loan after 2023-06-01? */
-SELECT user_name FROM user WHERE user_ID IN (SELECT user_ID FROM lending WHERE dateReturn IS NULL AND dateLending > '2023-06-01');
-
 /* What is the number of books published by the author with ID = 4? */
 SELECT COUNT(*) FROM book WHERE author_ID = 4;
-
-/* What are the names of authors with a book in the library and published before 2000? */
-SELECT author_name FROM author WHERE author_ID IN (SELECT author_ID FROM book WHERE book_ID IN (SELECT book_ID FROM library) AND ISBN < 2000);
-
-/* What are the titles of the books published by Nadia Ameziane Garcia that are in the library? */
-SELECT title FROM book WHERE author_ID = (SELECT author_ID FROM author WHERE author_name = 'Nadia Ameziane Garcia') AND book_ID IN (SELECT book_ID FROM library);
-
-/* What is the name of users with an outstanding loan between 2023-03-01 and 2023-06-01? */
-SELECT user_name FROM user WHERE user_ID IN (SELECT user_ID FROM lending WHERE dateReturn IS NULL AND dateLending BETWEEN '2023-03-01' AND '2023-06-01');
